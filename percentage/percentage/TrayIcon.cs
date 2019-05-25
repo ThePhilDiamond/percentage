826c3797b7fc9550d24fa71c17c15d808110e29e
David Colón <david@davidiq.com>
2019-05-24 22:41:27 -0400

    Fix code style and modify tooltip text

--- percentage/percentage/TrayIcon.cs
+++ percentage/percentage/TrayIcon.cs
@@ -9,58 +9,63 @@ namespace percentage
     {
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern bool DestroyIcon(IntPtr handle);
+
         private const string iconFont = "Roboto";
         private const int iconFontSize = 28;
-        private string batteryPercentage;
-        private NotifyIcon notifyIcon;
+        private const int refreshInterval = 1000; // in miliseconds
 
-        private int refreshInterval = 1000; // in miliseconds
+        private string batteryPercentage = "?";
+        private readonly NotifyIcon notifyIcon;
 
         public TrayIcon()
         {
-            ContextMenu contextMenu = new ContextMenu();
-            MenuItem menuItem = new MenuItem();
-
-            notifyIcon = new NotifyIcon();
+            // initialize menuItem
+            MenuItem menuItem = new MenuItem
+            {
+                Index = 0,
+                Text = "Exit"
+            };
+            menuItem.Click += new EventHandler(menuItem_Click);
 
             // initialize contextMenu
+            ContextMenu contextMenu = new ContextMenu();
             contextMenu.MenuItems.AddRange(new MenuItem[] { menuItem });
 
-            // initialize menuItem
-            menuItem.Index = 0;
-            menuItem.Text = "Exit";
-            menuItem.Click += new System.EventHandler(menuItem_Click);
-            notifyIcon.ContextMenu = contextMenu;
-            batteryPercentage = "?";
-            notifyIcon.Visible = true;
+            notifyIcon = new NotifyIcon
+            {
+                ContextMenu = contextMenu,
+                Visible = true,
+            };
 
-            Timer timer = new Timer();
+            Timer timer = new Timer { Interval = refreshInterval };
             timer.Tick += new EventHandler(timer_Tick);
-            timer.Interval = refreshInterval; 
             timer.Start();
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             PowerStatus powerStatus = SystemInformation.PowerStatus;
-            batteryPercentage = (powerStatus.BatteryLifePercent * 100).ToString();
-            
+            batteryPercentage = $"{(powerStatus.BatteryLifePercent * 100)}";
+
+
             using (Bitmap bitmap = new Bitmap(DrawText(batteryPercentage, new Font(iconFont, iconFontSize), Color.White, Color.Transparent)))
             {
-                System.IntPtr intPtr = bitmap.GetHicon();
+                IntPtr intPtr = bitmap.GetHicon();
                 try
                 {
                     using (Icon icon = Icon.FromHandle(intPtr))
                     {
+                        notifyIcon.Icon = icon;
+
                         // Offline means the laptop is not connected to a power source
-                        if (powerStatus.PowerLineStatus.ToString().Equals("Offline")) {
-                            notifyIcon.Icon = icon;
+                        if (powerStatus.PowerLineStatus == PowerLineStatus.Offline)
+                        {
                             var ts = TimeSpan.FromSeconds(powerStatus.BatteryLifeRemaining);
-                            
                             // if you don't want the leading zeros, you can replace the format by '{0}:{1}'
-                            notifyIcon.Text = string.Format("{0:00}:{1:00}", ts.Hours, ts.Minutes);
-                        } else {
-                            notifyIcon.Icon = icon;
+                            notifyIcon.Text = string.Format("{0:00}:{1:00} remaining", ts.Hours, ts.Minutes);
+                        }
+                        else
+                        {
                             notifyIcon.Text = "Charging";
                         }
                     }
@@ -79,32 +84,24 @@ namespace percentage
             Application.Exit();
         }
 
-        private Image DrawText(String text, Font font, Color textColor, Color backColor)
+        private Image DrawText(string text, Font font, Color textColor, Color backColor)
         {
-            var textSize = GetImageSize(text, font);
-            Image image = new Bitmap(50, 42);
+            Image image = new Bitmap(55, 42);
             using (Graphics graphics = Graphics.FromImage(image))
             {
                 // paint the background
                 graphics.Clear(backColor);
+                graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
 
                 // create a brush for the text
                 using (Brush textBrush = new SolidBrush(textColor))
                 {
-                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
                     graphics.DrawString(text, font, textBrush, 0, 0);
                     graphics.Save();
                 }
             }
 
             return image;
-        }
-
-        private static SizeF GetImageSize(string text, Font font)
-        {
-            using (Image image = new Bitmap(1, 1))
-            using (Graphics graphics = Graphics.FromImage(image))
-                return graphics.MeasureString(text, font);
         }
     }
 }
